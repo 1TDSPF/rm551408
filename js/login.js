44c2f60be1f7c9590f2a01d457dc6bbfc00172f0
jixatos <j.gody2004@gmail.com>
2023-05-18 12:10:34 -0300

    Acompanhamento de aula

--- js/login.js
+++ js/login.js
@@ -1,54 +1,102 @@
-//Criando lista de objetos
+//CRIAR UMA LISTA DE OBJETOS
 let listaDeUsuarios = [
     {
-        nomeCompleto: "Jaú da Selva",
-        usuarioEmail: "jar@gmail.com",
-        usuarioSenha: "123456"
+        nomeCompleto : "Jaú da Selva",
+        usuarioEmail : "jau@email.com",
+        usuarioSenha : "123456",
+        usuarioAvatar : "./img/aluno-juan.jpg"
     },
     {
-        nomeCompleto: "Paulo das Couves",
-        usuarioEmail: "paulo@gmail.com",
-        usuarioSenha: "123456"
+        nomeCompleto : "Paulo das Couves",
+        usuarioEmail : "paulo@email.com",
+        usuarioSenha : "123456",
+        usuarioAvatar : "./img/usuario-padrao.png"
     },
     {
-        nomeCompleto: "Maria do Socorro",
-        usuarioEmail: "maria@gmail.com",
-        usuarioSenha: "123456"
+        nomeCompleto : "José da Silva",
+        usuarioEmail : "jose@email.com",
+        usuarioSenha : "123456",
+        usuarioAvatar : "./img/usuario-padrao.png"
     },
     {
-        nomeCompleto: "André da Silva",
-        usuarioEmail: "andre@gmail.com",
-        usuarioSenha: "123456"
+        nomeCompleto : "Maria do Socorro",
+        usuarioEmail : "maria@email.com",
+        usuarioSenha : "123456",
+        usuarioAvatar : "./img/usuario-padrao.png"
+    },
+    {
+        nomeCompleto : "André da Silva",
+        usuarioEmail : "andre@email.com",
+        usuarioSenha : "123456",
+        usuarioAvatar : "./img/usuario-padrao.png"
     }
 ];
 
-localStorage.setItem("listaUser",JSON.stringify(listaDeUsuarios));
+//ADICIONANDO A LISTA DE OBJETOS AO LOCAL-STORAGE
+localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
 
 addEventListener("click", (evt)=>{
-
+    
     const inputEmail = document.querySelector("#idEmail");
     const inputPass = document.querySelector("#idPass");
+    
+    //CRIANDO UM OBJETO PARA ARMAZENAR OS DADOS QUE VIERAM DO FORM.
+    const userLogado = {
+        emailUserLogado : inputEmail.value,
+        senhaUserLogado : inputPass.value
+    }
+
+    //CRIANDO UM OBJETO PARA ARMAZENAR O USUÁRIO VALIDADO.
+    let userValidado = {};
 
     if(evt.target.id == "btnSubmit"){
+
         try {
+            
+            let listaDeUsuarios = JSON.parse(localStorage.getItem("listaUser"));
+
             listaDeUsuarios.forEach((usuario)=>{
-                if(inputEmail.value == usuario.usuarioEmail && inputPass.value == usuario.usuarioSenha) {
-                    throw("Validado");
+     
+                if(userLogado.emailUserLogado == usuario.usuarioEmail && userLogado.senhaUserLogado == usuario.usuarioSenha){
+                    userValidado = usuario;
+                    throw "VALIDADO";
                 }
             });
-            throw("Não validado");
+
+            throw "NÃO VALIDADO";
+
         } catch (msg) {
             const msgStatus = document.querySelector("#msgStatus");
-            if(msg == "Validado"){
-                msgStatus.setAttribute("style", "color:#00f000;");
-                msgStatus.innerHTML = "<span>Login efetuado com Sucesso!</strong></span>";
-            } else{
-                msgStatus.setAttribute("style", "color:#ff0000;");
-                msgStatus.innerHTML = "<span>Senha ou usuário inválidos!</strong></span>";
+            
+            if(msg == "VALIDADO"){
+                msgStatus.setAttribute("style","color:#00ff00;");
+                msgStatus.innerHTML = `<span><strong>O usuário ${userValidado.nomeCompleto} realizou o login com Sucesso!</strong></span>`;
+
+                //ADICIONANDO O USUÁRIO-VALIDADO NO LOCAL-STORAGE
+                localStorage.setItem("user-validado", JSON.stringify(userValidado));
+
+                //CRIAR UM TOKEN DE AUTENTICAÇÃO DO USUÁRIO
+                let token = Math.random().toString(16).substring(2)+Math.random().toString(16).substring(2);
+                console.log(token);
+
+                //ADICIONANDO O TOKEN NO LOCAL-STORAGE
+                localStorage.setItem("token-user", token);
+
+                //REDIRECT
+                setTimeout(()=>{
+                        window.location.href = "../index.html";
+                }, 3000);
+                
+            }else{
+                msgStatus.setAttribute("style","color:#ff0000;");
+                msgStatus.innerHTML = "<span><strong>Senha ou nome de usuário inválidos!</strong></span>";
             }
             
         }
+    //Ao mostrar a senha troque o ícone também pelo ícone de olho com um traço.
+
     }else if(evt.target.className == "fa fa-eye" || evt.target.className == "fa fa-eye-slash"){
+
         if(inputPass.getAttribute("type") == "password"){
             inputPass.setAttribute("type","text");
             evt.target.setAttribute("class","fa fa-eye-slash");
@@ -56,5 +104,6 @@ addEventListener("click", (evt)=>{
             inputPass.setAttribute("type","password");
             evt.target.setAttribute("class","fa fa-eye");
         }
+
     }
-}); 
+});
